999ab1a5698921c7e187645518e68bb3c122cd29
Loris89 <loristefano@hotmail.it>
2024-09-13 20:47:33 +0200

    Implemented POST endpoint to create a new product

--- eshop-microservices/src/Services/Catalog/Catalog.API/Models/Product.cs
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -10,5 +10,7 @@ public class Product
 
     public string Description { get; set; } = default!;
 
+    public string ImageFile { get; set; } = default!;
+
     public decimal Price { get; set; }
 }
