4036f5289a5c06c9f6e9b16123bf133709677304
Loris89 <loristefano@hotmail.it>
2024-10-05 14:06:09 +0200

    Added validators for commands

--- eshop-microservices/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ eshop-microservices/src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -15,11 +15,13 @@ public class CreateProductCommandValidator : AbstractValidator<CreateProductComm
     }
 }
 
-internal class CreateProductCommandHandler(IDocumentSession session)
+internal class CreateProductCommandHandler(IDocumentSession session, ILogger<CreateProductCommandHandler> logger)
     : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
+        logger.LogInformation("CreateProductCommandHandler.Handle called with {@Command}", command);
+
         var product = new Product
         {
             Name = command.Name,
