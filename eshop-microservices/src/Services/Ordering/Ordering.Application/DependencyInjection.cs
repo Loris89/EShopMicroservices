13ae01935b4837a85bb540f0b162d397f28a78cb
Loris89 <loristefano@hotmail.it>
2024-11-19 22:13:15 +0100

    Added interceptor for domain events

--- eshop-microservices/src/Services/Ordering/Ordering.Application/DependencyInjection.cs
+++ eshop-microservices/src/Services/Ordering/Ordering.Application/DependencyInjection.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace Ordering.Application;
 
@@ -7,6 +7,11 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+        });
+
         return services;
     }
 }
