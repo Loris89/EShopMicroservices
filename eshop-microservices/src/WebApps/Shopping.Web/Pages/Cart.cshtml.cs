ece8610cb5a9bb32ba3317560d6162ae11d5df35
Loris89 <loristefano@hotmail.it>
2025-01-29 20:38:31 +0100

    Cart page and model

--- eshop-microservices/src/WebApps/Shopping.Web/Pages/Cart.cshtml.cs
+++ eshop-microservices/src/WebApps/Shopping.Web/Pages/Cart.cshtml.cs
@@ -1,12 +1,25 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+namespace Shopping.Web.Pages;
 
-namespace Shopping.Web.Pages
+public class CartModel(IBasketService basketService, ILogger<CartModel> logger)
+    : PageModel
 {
-    public class CartModel : PageModel
+    public ShoppingCartModel Cart { get; set; } = new();
+
+    public async Task<IActionResult> OnGetAsync()
     {
-        public void OnGet()
-        {
-        }
+        Cart = await basketService.LoadUserBasket();
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostRemoveToCartAsync(Guid productId)
+    {
+        logger.LogInformation("Remove to cart button clicked");
+        Cart = await basketService.LoadUserBasket();
+
+        Cart.Items.RemoveAll(x => x.ProductId == productId);
+
+        await basketService.StoreBasket(new StoreBasketRequest(Cart));
+
+        return RedirectToPage();
     }
 }
